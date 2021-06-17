4953ba09cfbc587fa78e635e3cb9afca5ff34f1e
Faxel <74604677+threat0@users.noreply.github.com>
2021-06-17 20:56:27 +0000

    Add files via upload

---
+++ add/logo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+logo=("""
+  \x1b[38;5;197m═════════════════════════════════════
+     \033[38;5;214m░█▀▀▄ █▀▀█ █▀▀█ █─█ ─▀─ █▀▀▄ █▀▀▀
+     \033[38;5;214m░█─░█ █──█ █▄▄▀ █▀▄ ▀█▀ █──█ █─▀█
+     \033[38;5;214m░█▄▄▀ ▀▀▀▀ ▀─▀▀ ▀─▀ ▀▀▀ ▀──▀ ▀▀▀▀
+  \x1b[38;5;197m═════════════════════════════════════""")
+content = ("""\x1b[38;5;118m
+*--------------------------------------------*
+| GitHub Dorking (SANS TLD)       | 64 Lien  |
+| Messages d'erreur SQL et fuites | 216 Lien |
+| Extensions et types de fichiers | 221 Lien |
+| Index des fichiers              | 49 Lien  |
+| Vulnérabilités (SQLi & LFI)     | 30 Lien  |
+| Autres                          | 235 Lien |
+*--------------------------------------------*""")
+print(logo,content)
